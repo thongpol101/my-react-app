27e385e453ac55c6bfe95ca8839ff9452b5adf0f
thongpol101 <85303310+thongpol101@users.noreply.github.com>
2021-06-11 22:22:23 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-app-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" /
+        >
+        <Button> Test Button </Button > 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
