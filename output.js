cba4ade4cf1aa9ae4339e018a8719266f955fd7b
thongpol101 <85303310+thongpol101@users.noreply.github.com>
2021-06-11 23:45:22 +0700

    add

--- output.js
+++
@@ -1,6 +0,0 @@
-var flowRemoveTypes = require('flow-remove-types');
-var fs = require('fs');
-
-var input = fs.readFileSync('input.js', 'utf8');
-var output = flowRemoveTypes(input);
-fs.writeFileSync('output.js', output.toString());
